75b91f19408eb1226438e71e45b854124e8d7f2f
Jennifer Simond <jenniasimond@gmail.com>
2024-04-03 10:47:23 -0400

    add data.js file, update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
+import { useState } from 'react'
+import { puppyList } from "./data.js"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log('puppyList: ', puppyList)
 
   return (
     <>
-      <div>
-        
+      <div className='App'>
+        {puppies.map((puppy)=> {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+        }
       </div>
      
     </>
